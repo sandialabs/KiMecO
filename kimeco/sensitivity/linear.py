5d841d7bec42f914ba3bbc359b0808b729d3fb89
csoulie <csoulie@sandia.gov>
2026-07-21 10:56:23 -0700

    fix: fixed params now correctly applied to both sides of derivative during sensitivity analysis

--- kimeco/sensitivity/linear.py
+++ kimeco/sensitivity/linear.py
@@ -296,10 +296,11 @@ class Linear(CoreRun):
                     [f'{dbs}{Ptype.SCORE.value}']):
                     self.to_test.append(False)
                     continue
-                if key in self.settings['fixed_params'] and side == 1:
+                if key in self.settings['fixed_params']:
                     self.to_test.append(False)
-                    self.klog.info(
-                        f'Parameter {key} is fixed. Skipped.')
+                    if side == 1:
+                        self.klog.info(
+                            f'Parameter {key} is fixed. Skipped.')
                     continue
                 # Create a new SOP object with the modified parameter
                 self.to_test.append(True)
